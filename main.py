3e1ed98155bfc0bfcd4d539c2d37baa72ee62d72
Anusha-Patravali25 P <anushapatravali@gmail.com>
2025-04-29 08:23:59 +0000

    Complete medical data visualizer project

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-# This entrypoint file to be used in development. Start by reading README.md
-import medical_data_visualizer
-from unittest import main
+# # This entrypoint file to be used in development. Start by reading README.md
+# import medical_data_visualizer
+# from unittest import main
 
-# Test your function by calling it here
-medical_data_visualizer.draw_cat_plot()
-medical_data_visualizer.draw_heat_map()
+# # Test your function by calling it here
+# medical_data_visualizer.draw_cat_plot()
+# medical_data_visualizer.draw_heat_map()
 
-# Run unit tests automatically
-main(module='test_module', exit=False)
+# # Run unit tests automatically
+# main(module='test_module', exit=False)
+
+from medical_data_visualizer import draw_cat_plot, draw_heat_map
+
+# Run and save the plots
+cat_plot = draw_cat_plot()
+cat_plot.savefig('catplot.png')
+
+heat_map = draw_heat_map()
+heat_map.savefig('heatmap.png')
